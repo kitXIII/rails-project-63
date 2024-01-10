656d55be0b0af990ab7ced0a01a9638c679665d3
Nikita Konovalov <mipo4t@gmail.com>
2024-01-10 23:58:18 +0400

    add minitest-power_assert

--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -8,14 +8,14 @@ class TestHexletCode < Minitest::Test
   end
 
   def test_it_should_build_single_tag
-    assert HexletCode::Tag.build("br") == "<br>"
-    assert HexletCode::Tag.build("img", src: "path/to/image") == '<img src="path/to/image">'
-    assert HexletCode::Tag.build("input", type: "submit", value: "Save") == 'input type="submit" value="Save">'
+    assert { HexletCode::Tag.build("br") == "<br>" }
+    assert { HexletCode::Tag.build("img", src: "path/to/image") == '<img src="path/to/image">' }
+    assert { HexletCode::Tag.build("input", type: "submit", value: "Save") == '<input type="submit" value="Save">' }
   end
 
   def test_it_should_build_pair_tag
-    assert HexletCode::Tag.build("div") == "<div></div>"
-    assert HexletCode::Tag.build("label") { "Email" } == "<label>Email</label>"
-    assert HexletCode::Tag.build("label", for: "email") { "Email" } == '<label for="email">Email</label>'
+    assert { HexletCode::Tag.build("div") == "<div></div>" }
+    assert { HexletCode::Tag.build("label") { "Email" } == "<label>Email</label>" }
+    assert { HexletCode::Tag.build("label", for: "email") { "Email" } == '<label for="email">Email</label>' }
   end
 end
