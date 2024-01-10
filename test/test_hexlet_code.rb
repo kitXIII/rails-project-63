dff6f6b4f9ffea973efe9e668eb5f248b1d18c1f
Nikita Konovalov <mipo4t@gmail.com>
2024-01-10 21:47:07 +0400

    add tag build tests

--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -7,7 +7,15 @@ class TestHexletCode < Minitest::Test
     refute_nil ::HexletCode::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_it_should_build_single_tag
+    assert HexletCode::Tag.build("br") == "<br>"
+    assert HexletCode::Tag.build("img", src: "path/to/image") == '<img src="path/to/image">'
+    assert HexletCode::Tag.build("input", type: "submit", value: "Save") == 'input type="submit" value="Save">'
+  end
+
+  def test_it_should_build_pair_tag
+    assert HexletCode::Tag.build("div") == "<div></div>"
+    assert HexletCode::Tag.build("label") { "Email" } == "<label>Email</label>"
+    assert HexletCode::Tag.build("label", for: "email") { "Email" } == '<label for="email">Email</label>'
   end
 end
